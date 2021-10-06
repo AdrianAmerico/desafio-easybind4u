6d2d572705db37b01b8ea943a9e2550445ded27e
Adrian Américo <1fnadrian1@gmail.com>
2021-10-06 13:06:31 -0300

    corrigido problemas de container e inserido novas features

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/Header";
 import UserInfo from "./tabs/UserInfo/UserInfo";
 import { useStyles } from "./styles";
 import Footer from "../../components/Footer";
+import UserData from "./tabs/UserData/UserData";
 
 const RegisterPage: React.FC = () => {
+  const [isSecondStep, setIsSecondStep] = useState<boolean>(false);
+
+  const changeSecondStep = (): void => {
+    setIsSecondStep(true);
+  };
+
   const classes = useStyles();
   return (
     <>
       <Header />
       <main className={classes.root}>
-        <UserInfo />
+        {isSecondStep ? <UserData /> : <UserInfo />}
       </main>
       <Footer />
     </>
--- src/pages/RegisterPage/styles.ts
+++ src/pages/RegisterPage/styles.ts
@@ -5,7 +5,7 @@ export const useStyles = makeStyles((theme: Theme) =>
     root: {
       width: "100%",
       height: "100%",
-      flex: "1",
+      flex: "8",
       padding: theme.spacing(2, 0),
     },
   })
